7127a370a45e29e6351664ee3006755cd187c0ff
FelipeBacab <felipe.19070004@itsmotul.edu.mx>
2023-02-09 17:33:17 -0600

    Tutorial 3

--- Tutorial 3/styles.ts
+++ Tutorial 3/styles.ts
@@ -1,31 +1,13 @@
-<script type="module" src="./motion-carousel.js"></script>
+import {css} from 'lit';
 
-<h2>motion-carousel</h2>
-<motion-carousel id="carousel">
-  <img src="https://picsum.photos/seed/1/200">
-  <img src="https://picsum.photos/seed/2/200">
-  <img src="https://picsum.photos/seed/3/200">
-  <img src="https://picsum.photos/seed/4/200">
-  <img src="https://picsum.photos/seed/5/200">
-  <img src="https://picsum.photos/seed/6/200">
-</motion-carousel>
-
-<style>
-  body {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-family: sans-serif;
+export const styles = css`
+  :host {
   }
 
-  #carousel {
-    width: 200px;
-    height: 200px;
-    user-select: none;
+  .fit {
   }
 
-  #carousel:not(:defined) > * {
-    display: none;
+  ::slotted(*) {
   }
-</style>
+`;
 
