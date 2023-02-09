4928dc7a0a6f88a4667cd5e57440635fa4df8bfd
FelipeBacab <felipe.19070004@itsmotul.edu.mx>
2023-02-09 15:41:37 -0600

    Tutorial 1

--- Tutorial 1/my-element.ts
+++ Tutorial 1/my-element.ts
@@ -3,13 +3,12 @@ import {customElement, property} from 'lit/decorators.js';
 
 @customElement('my-element')
 export class MyElement extends LitElement {
-  @property()
-  version = 'STARTING';
+// TODO: Add a reactive property
 
   render() {
     return html`
-    <p>Welcome to the Lit tutorial!</p>
-    <p>This is the ${this.version} code.</p>
+      <p>TODO: Add an expression</p>
     `;
   }
 }
+
